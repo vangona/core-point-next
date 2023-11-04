1e659bcd8501f8339d2197d1ef373e92d6b480ad
Kwan-Kyung Kim <vincent-kim@Kwan-Kyungs-MacBook-Air.local>
2023-11-04 19:46:23 +0900

    chore: 스토리북 public 경로 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,6 +3,7 @@ import path from 'path';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
