3ac9b3a87bf53828c1edbfff471b911692e124da
vangona <rlarhksrud12@naver.com>
2023-11-06 01:53:21 +0900

    chore: 라우팅 경로 추가

---
+++ src/constants/routes.ts
@@ -0,0 +1,7 @@
+export enum CorePointRoutes {
+  HOME = '/',
+  STORE = '/store',
+  OPENING_CONSULTING = '/consulting/opening',
+  TRANSFER_CONSULTING = '/consulting/transfer',
+  PARTNERSHIP = '/partnership',
+}
