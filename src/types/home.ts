3c39aa43060518b7ff7c247bc309658427570e4b
vangona <rlarhksrud12@naver.com>
2023-11-11 18:38:46 +0900

    chore: Home 타입 수정

--- src/types/home.ts
+++ src/types/home.ts
@@ -1,11 +1,27 @@
 import { HeroData } from './metadata';
+import { Store } from './store';
 
 export type MainHeroCaroselData = HeroData[];
+
+export interface RecommendedStoreData {
+  recommendedStoreData: Store[];
+  limit: number;
+}
 
 export interface SuccessExample {
   title: string;
   content: string;
   imgSrc: string;
+}
+
+export interface SuccessExampleData {
+  successExampleData: SuccessExample[];
+  limit: number;
+}
+
+export interface LatestStoreData {
+  latestStoreData: Store[];
+  limit: number;
 }
 
 export interface BrandData {
