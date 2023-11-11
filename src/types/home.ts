bd3aceaf63223ebdc8042a447c539e0e6b8f2590
vangona <rlarhksrud12@naver.com>
2023-11-11 18:06:09 +0900

    chore: Home 페이지 타입 설계

---
+++ src/types/home.ts
@@ -0,0 +1,19 @@
+export interface MainHeroData {
+  title: string;
+  description: string;
+  imgSrc: string;
+}
+
+export type MainHeroCaroselData = MainHeroData[];
+
+export interface SuccessExample {
+  title: string;
+  content: string;
+  imgSrc: string;
+}
+
+export interface BrandData {
+  brandId: string;
+  brandName: string;
+  imgSrc: string;
+}
