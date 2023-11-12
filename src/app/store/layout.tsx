3db6ba55dffc68d7af8098acb749aaf81c758159
vangona <rlarhksrud12@naver.com>
2023-11-12 22:00:36 +0900

    refactor: store 구조 수정

---
+++ src/app/store/layout.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import '@/app/globals.css';
+import { Box } from '@mui/material';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '코어창업 | 매물 정보',
+  description: '코어창업 매물 정보',
+};
+
+export default function StoreLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 3,
+      }}
+    >
+      {children}
+    </Box>
+  );
+}
