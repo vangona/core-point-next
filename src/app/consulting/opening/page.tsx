06c8bc561930487002b293235ac122ca6acf1eaf
vangona <rlarhksrud12@naver.com>
2023-11-13 04:12:59 +0900

    feat: 전체 폼 대략적으로 퍼블리싱

--- src/app/consulting/opening/page.tsx
+++ src/app/consulting/opening/page.tsx
@@ -1,3 +1,34 @@
+import { Box, Button, TextField } from '@mui/material';
+import { DEFAULT_LAYOUT_WIDTH } from '@/components/layout/general-layout/constants';
+
 export default function Opening() {
-  return 'Opening';
+  return (
+    <Box
+      sx={{
+        width: DEFAULT_LAYOUT_WIDTH,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: 5,
+          alignItems: 'flex-start',
+          border: '1px solid',
+          borderColor: 'divider',
+          borderRadius: 2,
+        }}
+      >
+        <TextField label='성함' variant='outlined' />
+        <TextField label='연락처' variant='outlined' />
+        <TextField label='창업 희망지역' variant='outlined' />
+        <TextField label='예상 창업금액' variant='outlined' />
+        <TextField label='희망업종' variant='outlined' />
+        <TextField label='문의사항' variant='outlined' multiline fullWidth />
+        <Button size='large' variant='contained'>
+          컨설팅 신청하기
+        </Button>
+      </Box>
+    </Box>
+  );
 }
--- src/app/partnership/page.tsx
+++ src/app/partnership/page.tsx
@@ -1,3 +1,26 @@
+import { Box } from '@mui/material';
+import { DEFAULT_LAYOUT_WIDTH } from '@/components/layout/general-layout/constants';
+
 export default function Partnership() {
-  return 'Partnership';
+  return (
+    <Box
+      sx={{
+        width: DEFAULT_LAYOUT_WIDTH,
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          padding: 5,
+          alignItems: 'flex-start',
+          border: '1px solid',
+          borderColor: 'divider',
+          borderRadius: 2,
+        }}
+      >
+        내용 확인 필요함
+      </Box>
+    </Box>
+  );
 }
