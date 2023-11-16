9826e5bb60be1eec4f683d1d8772b2c44d3c2200
vangona <rlarhksrud12@naver.com>
2023-11-16 22:44:51 +0900

    feat: GET Store 구현

---
+++ src/app/api/store/route.ts
@@ -0,0 +1,14 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { supabase } from '@/api/supabase';
+import { SupabaseTable } from '@/api/types';
+import { DEFAULT_LIMIT, DEFAULT_PAGE } from './constants';
+
+export async function GET(req: NextRequest) {
+  const page = parseInt(req.nextUrl.searchParams.get('page') ?? DEFAULT_PAGE); // 빈 값이면 첫 페이지인 0
+  const limit = parseInt(req.nextUrl.searchParams.get('limt') ?? DEFAULT_LIMIT); // 빈 값이면 기본값인 20
+  const query = await supabase
+    .from(SupabaseTable.STORES)
+    .select('*')
+    .range(page * limit, (page + 1) * limit);
+  return NextResponse.json({ data: query.data });
+}
