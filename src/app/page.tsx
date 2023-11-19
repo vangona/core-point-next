6f757857ac3fe88496a94f4df59cc83112bab26b
vangona <rlarhksrud12@naver.com>
2023-11-20 03:53:17 +0900

    feat: 메인페이지 하단 반응형 적용

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useMediaQuery, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -12,6 +13,8 @@ import { SectionLayout } from '@/components/main/section-layout';
 import SuccessExample from '@/components/main/success-example-store/SuccessExampleStore';
 
 export default function Home() {
+  const theme = useTheme();
+  const isDownMedium = useMediaQuery(theme.breakpoints.down('md'));
   const { data, isLoading } = useGetStore({ page: '1', limit: '20' });
 
   return (
@@ -32,12 +35,35 @@ export default function Home() {
         <SectionTitle label='협업 브랜드' />
       </SectionLayout>
       <SectionLayout color='white' innerSx={{ gap: 8 }}>
-        <Typography variant='h4' component='h3' fontWeight='bold'>
-          {'창업의 문을 여는 열쇠, '}
-          <Box display='inline' color='primary.main'>
-            코어창업
+        {isDownMedium ? (
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              gap: 2,
+            }}
+          >
+            <Typography variant='h4' component='h3' fontWeight='bold'>
+              창업의 문을 여는 열쇠
+            </Typography>
+            <Typography
+              variant='h4'
+              component='h3'
+              fontWeight='bold'
+              color='primary.main'
+            >
+              코어창업
+            </Typography>
           </Box>
-        </Typography>
+        ) : (
+          <Typography variant='h4' component='h3' fontWeight='bold'>
+            {'창업의 문을 여는 열쇠, '}
+            <Box display='inline' color='primary.main'>
+              코어창업
+            </Box>
+          </Typography>
+        )}
         <Button variant='contained' size='large' sx={{ fontWeight: 'bold' }}>
           상담 신청하기
         </Button>
