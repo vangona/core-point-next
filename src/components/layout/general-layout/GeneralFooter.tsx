fa332360883d94665ad3b1c0e56962a51d00ccfa
vangona <rlarhksrud12@naver.com>
2023-11-19 23:48:02 +0900

    feat: Footer 구현

--- src/components/layout/general-layout/GeneralFooter.tsx
+++ src/components/layout/general-layout/GeneralFooter.tsx
@@ -1,6 +1,9 @@
 import Box from '@mui/material/Box';
+import Divider from '@mui/material/Divider';
+import { default as StyledLink } from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
 import Image from 'next/image';
-import { DIMMED_GRAY, OFF_WHITE_COLOR } from '@/constants/color';
+import Link from 'next/link';
 import { DEFAULT_FOOTER_HEIGHT, DEFAULT_LAYOUT_WIDTH } from './constants';
 
 const GeneralFooter = () => {
@@ -12,7 +15,7 @@ const GeneralFooter = () => {
         display: 'flex',
         justifyContent: 'center',
         alignItems: 'center',
-        background: `linear-gradient(${OFF_WHITE_COLOR}, ${DIMMED_GRAY})`,
+        backgroundColor: 'primary.main',
       }}
     >
       <Box
@@ -22,17 +25,105 @@ const GeneralFooter = () => {
           display: 'flex',
           flexDirection: 'column',
           alignItems: 'center',
+          justifyContent: 'center',
+          gap: 3,
+          color: 'primary.contrastText',
         }}
       >
-        <Box>
-          <Image
-            width={120}
-            height={60}
-            src='/logo.png'
-            alt='열쇠 모양에 Core Company라는 글자가 적혀있는 코어 창업 로고'
-          />
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center ',
+            gap: 1,
+          }}
+        >
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <Image
+              width={30}
+              height={30}
+              src='/core-icon-white.svg'
+              alt='열쇠 모양에 Core Company라는 글자가 적혀있는 코어 창업 로고'
+            />
+            <Typography variant='subtitle1'>Core Point</Typography>
+          </Box>
+          <Typography variant='caption'>
+            &copy; {new Date().getFullYear()} 코어컴퍼니
+          </Typography>
         </Box>
-        <Box>Company Info</Box>
+        <Box
+          sx={{
+            display: 'flex',
+            gap: 5,
+            justifyContent: 'space-evenly',
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1,
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant='subtitle2'>회사정보</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <Typography variant='caption'>사업자명 | 코어컴퍼니</Typography>
+              <Typography variant='caption'>대표 | 조석훈</Typography>
+            </Box>
+          </Box>
+          <Divider
+            orientation='vertical'
+            flexItem
+            sx={{ height: '100%', backgroundColor: 'primary.dark' }}
+          />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1,
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant='subtitle2'>연락처</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <Typography variant='caption'>
+                전화번호 | 010-4537-5739
+              </Typography>
+              <Typography variant='caption'>
+                이메일 | corepoint0905@naver.com
+              </Typography>
+            </Box>
+          </Box>
+          <Divider
+            orientation='vertical'
+            flexItem
+            sx={{ height: '100%', backgroundColor: 'primary.dark' }}
+          />
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 1,
+              alignItems: 'center',
+            }}
+          >
+            <Typography variant='subtitle2'>SNS</Typography>
+            <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+              <Link legacyBehavior href='https://blog.naver.com/corepoint_'>
+                <StyledLink color='primary.contrastText' variant='caption'>
+                  네이버 블로그
+                </StyledLink>
+              </Link>
+              <Link legacyBehavior href='https://open.kakao.com/o/stsH1PSf'>
+                <StyledLink color='primary.contrastText' variant='caption'>
+                  카카오톡 오픈채팅
+                </StyledLink>
+              </Link>
+            </Box>
+          </Box>
+        </Box>
       </Box>
     </Box>
   );
--- src/components/layout/general-layout/constants.ts
+++ src/components/layout/general-layout/constants.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_HEADER_HEIGHT = '100px';
 export const DEFAULT_LAYOUT_WIDTH = '1200px';
 export const DEFAULT_HERO_HEIGHT = '500px';
-export const DEFAULT_FOOTER_HEIGHT = '300px';
+export const DEFAULT_FOOTER_HEIGHT = '200px';
