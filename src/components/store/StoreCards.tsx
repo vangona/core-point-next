a88520d8f8fae7661e5eecfe94c58cd196d02e2a
vangona <rlarhksrud12@naver.com>
2023-11-24 18:18:50 +0900

    feat: 매물 검색결과 개수 표시

--- src/components/store/StoreCards.tsx
+++ src/components/store/StoreCards.tsx
@@ -2,6 +2,7 @@
 
 import { useMediaQuery, useTheme } from '@mui/material';
 import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { Store } from '@/api/store';
 import { StoreCard } from '@/components/common/store-card';
 import { MobileStoreCard } from '../common/mobile-store-card';
@@ -22,41 +23,44 @@ const StoreCards = ({
   const isDownMedium = useMediaQuery(theme.breakpoints.down('md'));
 
   return (
-    <Box
-      sx={{
-        width: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: isDownMedium ? 1 : 3,
-      }}
-    >
-      {Array.isArray(storeData) &&
-        storeData.length === 0 &&
-        '조건에 해당하는 매물이 없습니다. 조건을 변경하여 다시 검색해주세요.'}
-      {!isDownMedium &&
-        Array.isArray(storeData) &&
-        storeData.length > 0 &&
-        storeData.map((store, index) => (
-          <StoreCard
-            key={'store-card' + index + ', id-' + store.store_id}
-            storeData={store}
-            handleStoreChange={handleStoreChange}
-            openModal={openModal}
-            onCardClick={onCardClick}
-          />
-        ))}
-      {isDownMedium &&
-        Array.isArray(storeData) &&
-        storeData.length > 0 &&
-        storeData.map((store, index) => (
-          <MobileStoreCard
-            key={'store-card' + index + ', id-' + store.store_id}
-            storeData={store}
-            handleStoreChange={handleStoreChange}
-            openModal={openModal}
-            onCardClick={onCardClick}
-          />
-        ))}
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 2 }}>
+      <Typography>{storeData.length}개의 검색결과</Typography>
+      <Box
+        sx={{
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: isDownMedium ? 1 : 3,
+        }}
+      >
+        {Array.isArray(storeData) &&
+          storeData.length === 0 &&
+          '조건에 해당하는 매물이 없습니다. 조건을 변경하여 다시 검색해주세요.'}
+        {!isDownMedium &&
+          Array.isArray(storeData) &&
+          storeData.length > 0 &&
+          storeData.map((store, index) => (
+            <StoreCard
+              key={'store-card' + index + ', id-' + store.store_id}
+              storeData={store}
+              handleStoreChange={handleStoreChange}
+              openModal={openModal}
+              onCardClick={onCardClick}
+            />
+          ))}
+        {isDownMedium &&
+          Array.isArray(storeData) &&
+          storeData.length > 0 &&
+          storeData.map((store, index) => (
+            <MobileStoreCard
+              key={'store-card' + index + ', id-' + store.store_id}
+              storeData={store}
+              handleStoreChange={handleStoreChange}
+              openModal={openModal}
+              onCardClick={onCardClick}
+            />
+          ))}
+      </Box>
     </Box>
   );
 };
