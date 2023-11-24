36e799e5f5db094b1a0df3e82a40e97eafc6d957
vangona <rlarhksrud12@naver.com>
2023-11-24 20:25:14 +0900

    feat: 협업신청 API Supabase DB 연결

--- src/app/api/partnership/route.ts
+++ src/app/api/partnership/route.ts
@@ -4,6 +4,7 @@ import { supabase } from '@/app/api/supabase';
 import { PartnershipFormInput } from '@/components/partnership/PartnershipForm';
 import {
   PartnershipBrandsColumn,
+  PartnershipRequestColumn,
   StoreState,
   SupabaseBoolean,
   SupabaseTable,
@@ -12,18 +13,28 @@ import {
 export async function POST(req: NextRequest) {
   const body = (await req.json()) as PartnershipFormInput;
 
+  const query = await supabase.from(SupabaseTable.PARTNERSHIP_REQUEST).insert([
+    {
+      [PartnershipRequestColumn.NAME]: body.name,
+      [PartnershipRequestColumn.CONTACT]: body.contact,
+      [PartnershipRequestColumn.BRAND_NAME]: body.brandName,
+      [PartnershipRequestColumn.REQUEST_STATE]: StoreState.WAIT,
+      [PartnershipRequestColumn.ADDITIONAL]: body.additional,
+    },
+  ]);
+
   resend.emails.send({
     from: '코어창업 <noreply@core-point.kr>',
     to: ['corepoint0905@naver.com'],
     subject: `새로운 협업 신청이 접수되었습니다. ${body.name}님, ${body.brandName} 건`,
     text: `담당자 성함 : ${body.name}
-담당자 연락처 : ${body.contact}
-브랜드명 : ${body.brandName}
-추가 문의사항 : ${body.additional ? body.additional : '없음'}   
-`,
+  담당자 연락처 : ${body.contact}
+  브랜드명 : ${body.brandName}
+  추가 문의사항 : ${body.additional ? body.additional : '없음'}
+  `,
   });
 
-  return NextResponse.json({ status: 'good' });
+  return NextResponse.json({ data: query.data });
 }
 
 export async function GET() {
