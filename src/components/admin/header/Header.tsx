700a8da70e3d7e9653a1366abedfac7d9b5ed5a0
vangona <rlarhksrud12@naver.com>
2023-11-29 23:18:45 +0900

    chore: 관리자페이지 Header 정리

--- src/components/admin/header/Header.tsx
+++ src/components/admin/header/Header.tsx
@@ -1,20 +1,12 @@
 import * as React from 'react';
-import HelpIcon from '@mui/icons-material/Help';
 import MenuIcon from '@mui/icons-material/Menu';
-import NotificationsIcon from '@mui/icons-material/Notifications';
 import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import IconButton from '@mui/material/IconButton';
-import Link from '@mui/material/Link';
 import Tab from '@mui/material/Tab';
 import Tabs from '@mui/material/Tabs';
 import Toolbar from '@mui/material/Toolbar';
-import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
-
-const lightColor = 'rgba(255, 255, 255, 0.7)';
 
 interface HeaderProps {
   onDrawerToggle: () => void;
@@ -27,48 +19,15 @@ export default function Header(props: HeaderProps) {
     <React.Fragment>
       <AppBar color='primary' position='sticky' elevation={0}>
         <Toolbar>
-          <Grid container spacing={1} alignItems='center'>
-            <Grid sx={{ display: { sm: 'none', xs: 'block' } }} item>
-              <IconButton
-                color='inherit'
-                aria-label='open drawer'
-                onClick={onDrawerToggle}
-                edge='start'
-              >
-                <MenuIcon />
-              </IconButton>
-            </Grid>
-            <Grid item xs />
-            <Grid item>
-              <Link
-                href='/'
-                variant='body2'
-                sx={{
-                  textDecoration: 'none',
-                  color: lightColor,
-                  '&:hover': {
-                    color: 'common.white',
-                  },
-                }}
-                rel='noopener noreferrer'
-                target='_blank'
-              >
-                Go to docs
-              </Link>
-            </Grid>
-            <Grid item>
-              <Tooltip title='Alerts • No alerts'>
-                <IconButton color='inherit'>
-                  <NotificationsIcon />
-                </IconButton>
-              </Tooltip>
-            </Grid>
-            <Grid item>
-              <IconButton color='inherit' sx={{ p: 0.5 }}>
-                <Avatar src='/static/images/avatar/1.jpg' alt='My Avatar' />
-              </IconButton>
-            </Grid>
-          </Grid>
+          <IconButton
+            color='inherit'
+            aria-label='open drawer'
+            onClick={onDrawerToggle}
+            edge='start'
+            sx={{ display: { sm: 'none', xs: 'block' } }}
+          >
+            <MenuIcon />
+          </IconButton>
         </Toolbar>
       </AppBar>
       <AppBar
@@ -84,23 +43,6 @@ export default function Header(props: HeaderProps) {
               <Typography color='inherit' variant='h5' component='h1'>
                 Authentication
               </Typography>
-            </Grid>
-            <Grid item>
-              <Button
-                sx={{ borderColor: lightColor }}
-                variant='outlined'
-                color='inherit'
-                size='small'
-              >
-                Web setup
-              </Button>
-            </Grid>
-            <Grid item>
-              <Tooltip title='Help'>
-                <IconButton color='inherit'>
-                  <HelpIcon />
-                </IconButton>
-              </Tooltip>
             </Grid>
           </Grid>
         </Toolbar>
