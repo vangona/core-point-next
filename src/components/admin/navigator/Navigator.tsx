3c5ed5006fb3a1ee1b856eea01041fa0a5ccb4cc
vangona <rlarhksrud12@naver.com>
2023-11-29 23:16:00 +0900

    chore: 관리자페이지 메뉴 수정

--- src/components/admin/navigator/Navigator.tsx
+++ src/components/admin/navigator/Navigator.tsx
@@ -1,13 +1,7 @@
-import DnsRoundedIcon from '@mui/icons-material/DnsRounded';
 import HomeIcon from '@mui/icons-material/Home';
-import PeopleIcon from '@mui/icons-material/People';
-import PhonelinkSetupIcon from '@mui/icons-material/PhonelinkSetup';
 import PermMediaOutlinedIcon from '@mui/icons-material/PhotoSizeSelectActual';
 import PublicIcon from '@mui/icons-material/Public';
-import SettingsIcon from '@mui/icons-material/Settings';
-import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
-import SettingsInputComponentIcon from '@mui/icons-material/SettingsInputComponent';
-import TimerIcon from '@mui/icons-material/Timer';
+import StoreMallDirectoryRounded from '@mui/icons-material/StoreMallDirectoryRounded';
 import Box from '@mui/material/Box';
 import Divider from '@mui/material/Divider';
 import Drawer from '@mui/material/Drawer';
@@ -16,33 +10,33 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
+import Link from 'next/link';
 import type { DrawerProps } from '@mui/material/Drawer';
 
 const categories = [
   {
-    id: '사이트 정보',
+    id: '매물 정보',
     children: [
       {
-        id: 'Authentication',
-        icon: <PeopleIcon />,
-        active: true,
-      },
-      { id: 'Database', icon: <DnsRoundedIcon /> },
-      { id: 'Storage', icon: <PermMediaOutlinedIcon /> },
-      { id: 'Hosting', icon: <PublicIcon /> },
-      { id: 'Functions', icon: <SettingsEthernetIcon /> },
-      {
-        id: 'Machine learning',
-        icon: <SettingsInputComponentIcon />,
+        id: '매물정보 관리',
+        icon: <StoreMallDirectoryRounded />,
+        href: '/admin/store',
       },
     ],
   },
   {
-    id: '매물 정보',
+    id: '사이트 정보',
     children: [
-      { id: 'Analytics', icon: <SettingsIcon /> },
-      { id: 'Performance', icon: <TimerIcon /> },
-      { id: 'Test Lab', icon: <PhonelinkSetupIcon /> },
+      {
+        id: '메인 페이지 관리',
+        icon: <PermMediaOutlinedIcon />,
+        href: '/admin/main',
+      },
+      {
+        id: '사이트 하단 정보 관리',
+        icon: <PublicIcon />,
+        href: '/admin/footer',
+      },
     ],
   },
 ];
@@ -77,19 +71,21 @@ export default function Navigator(props: DrawerProps) {
           <ListItemIcon>
             <HomeIcon />
           </ListItemIcon>
-          <ListItemText>Project Overview</ListItemText>
+          <ListItemText>웹사이트 개요</ListItemText>
         </ListItem>
         {categories.map(({ id, children }) => (
           <Box key={id} sx={{ bgcolor: '#101F33' }}>
             <ListItem sx={{ py: 2, px: 3 }}>
               <ListItemText sx={{ color: '#fff' }}>{id}</ListItemText>
             </ListItem>
-            {children.map(({ id: childId, icon, active }) => (
+            {children.map(({ id: childId, icon, href }) => (
               <ListItem disablePadding key={childId}>
-                <ListItemButton selected={active} sx={item}>
-                  <ListItemIcon>{icon}</ListItemIcon>
-                  <ListItemText>{childId}</ListItemText>
-                </ListItemButton>
+                <Link href={href} style={{ textDecoration: 'none' }}>
+                  <ListItemButton sx={item}>
+                    <ListItemIcon>{icon}</ListItemIcon>
+                    <ListItemText>{childId}</ListItemText>
+                  </ListItemButton>
+                </Link>
               </ListItem>
             ))}
             <Divider sx={{ mt: 2 }} />
