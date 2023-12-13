771a85927787753eb96939fc4d2f433187e700a3
vangona <rlarhksrud12@naver.com>
2023-12-13 18:06:30 +0900

    refactor: mobile description 분리

--- src/components/admin/store/edit-dialog/StoreMobileDescription.tsx
+++ src/components/admin/store/edit-dialog/StoreMobileDescription.tsx
@@ -1,7 +1,87 @@
-import React from 'react';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import { useQueryClient, useMutation } from '@tanstack/react-query';
+import {
+  PatchStoreMobileDescriptionBody,
+  patchStoreMobileDescription,
+} from '@/api/store/patchStoreMobileDescription';
+import { Store } from '@/api/store/types';
+import type { AlertColor } from '@mui/material';
 
-const StoreMobileDescription = () => {
-  return <div></div>;
+interface StoreMobileDescriptionProps {
+  data?: Store;
+  setIsSnackbar: Dispatch<SetStateAction<boolean>>;
+  setSnackbarStatus: Dispatch<SetStateAction<AlertColor | undefined>>;
+  setSnackbarTitle: Dispatch<SetStateAction<string>>;
+}
+const StoreMobileDescription = ({
+  data,
+  setIsSnackbar,
+  setSnackbarStatus,
+  setSnackbarTitle,
+}: StoreMobileDescriptionProps) => {
+  const queryClient = useQueryClient();
+  const [editedMobileDescription, setEditedMobileDescription] =
+    useState<string>('');
+
+  const { mutate: updateMobileDescription } = useMutation<
+    unknown,
+    Error,
+    PatchStoreMobileDescriptionBody
+  >({
+    mutationKey: ['stores'],
+    mutationFn: (variables) => patchStoreMobileDescription(variables),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['stores'] });
+      setIsSnackbar(true);
+      setSnackbarStatus('success');
+      setSnackbarTitle('모바일 설명 수정에 성공했습니다.');
+    },
+    onError: () => {
+      setIsSnackbar(true);
+      setSnackbarStatus('error');
+      setSnackbarTitle('모바일 설명 수정에 문제가 발생했습니다.');
+    },
+  });
+
+  const handleMobileDescriptionSubmit = (
+    descriptionBody: PatchStoreMobileDescriptionBody,
+  ) => {
+    if (descriptionBody.mobile_description === data?.mobile_description) return;
+    updateMobileDescription(descriptionBody);
+  };
+
+  useEffect(() => {
+    if (!data) return;
+    setEditedMobileDescription(data.mobile_description ?? '');
+  }, [data]);
+
+  return (
+    <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+      <TextField
+        label='모바일 설명'
+        multiline
+        sx={{ mt: 2, width: '100%' }}
+        minRows={10}
+        onChange={(e) => setEditedMobileDescription(e.target.value)}
+        value={editedMobileDescription}
+      />
+      <Button
+        sx={{ alignSelf: 'flex-end' }}
+        onClick={() => {
+          if (!data) return;
+          handleMobileDescriptionSubmit({
+            id: data.store_id,
+            mobile_description: editedMobileDescription,
+          });
+        }}
+      >
+        모바일 설명 저장
+      </Button>
+    </Box>
+  );
 };
 
 export default StoreMobileDescription;
