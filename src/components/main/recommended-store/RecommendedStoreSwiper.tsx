1a9cc3eb3e30427b07350fa6bf76837916a95a07
vangona <rlarhksrud12@naver.com>
2023-12-14 18:57:07 +0900

    chore: flip으로 변경

--- src/components/main/recommended-store/RecommendedStoreSwiper.tsx
+++ src/components/main/recommended-store/RecommendedStoreSwiper.tsx
@@ -1,24 +1,40 @@
 import { useEffect, useRef, useState } from 'react';
 import { useMediaQuery, useTheme } from '@mui/material';
+import Box from '@mui/material/Box';
 import Skeleton from '@mui/material/Skeleton';
 import { useSuspenseQuery } from '@tanstack/react-query';
-import { Autoplay, Mousewheel, Navigation, Pagination } from 'swiper/modules';
+import {
+  Autoplay,
+  EffectFlip,
+  Mousewheel,
+  Navigation,
+  Pagination,
+} from 'swiper/modules';
 import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import { getRecommendedStore } from '@/api/store/getRecommendedStore';
 import ProgressBackdrop from '@/components/common/progress-backdrop/ProgressBackdrop';
 import { SwiperPrevButton, SwiperNextButton } from '@/components/common/swiper';
 import { VerticalStoreCard } from '@/components/common/vertical-store-card';
+import {
+  LARGE_LAYOUT_WIDTH,
+  MEDIUM_LAYOUT_WIDTH,
+  SMALL_LAYOUT_WIDTH,
+} from '@/components/layout/general-layout/constants';
 
-const LARGE_SLIDE_PER_VIEW = 4;
-const MEDIUM_SLIDE_PER_VIEW = 2;
-const SMALL_SLIDE_PER_VIEW = 1;
+const LARGE_SWIPER_WRAPPER_WIDTH = 500;
+const SMALL_SWIPER_WRAPPER_WIDTH = 350;
 
 const RecommendedStoreSwiper = () => {
   const theme = useTheme();
   const isUpLarge = useMediaQuery(theme.breakpoints.up('lg'));
   const isMedium = useMediaQuery(theme.breakpoints.only('md'));
   const isDownMedium = useMediaQuery(theme.breakpoints.down('md'));
-  const [slidePerView, setSlidePerView] = useState(LARGE_SLIDE_PER_VIEW);
+  const [slideWrapperWidth, setSliderWrapperWidth] = useState(
+    LARGE_SWIPER_WRAPPER_WIDTH,
+  );
+  const [layoutWidth, setLayoutWidth] = useState<string | number>(
+    LARGE_LAYOUT_WIDTH,
+  );
 
   const [isBackdrop, setIsBackdrop] = useState(false);
 
@@ -35,47 +51,71 @@ const RecommendedStoreSwiper = () => {
 
   useEffect(() => {
     if (isUpLarge) {
-      setSlidePerView(LARGE_SLIDE_PER_VIEW);
+      setSliderWrapperWidth(LARGE_SWIPER_WRAPPER_WIDTH);
+      setLayoutWidth(LARGE_LAYOUT_WIDTH);
       return;
     }
 
     if (isMedium) {
-      setSlidePerView(MEDIUM_SLIDE_PER_VIEW);
+      setLayoutWidth(MEDIUM_LAYOUT_WIDTH);
       return;
     }
 
-    setSlidePerView(SMALL_SLIDE_PER_VIEW);
+    setSliderWrapperWidth(SMALL_SWIPER_WRAPPER_WIDTH);
+    setLayoutWidth(SMALL_LAYOUT_WIDTH);
   }, [isUpLarge, isMedium]);
 
   return (
-    <>
-      {!isDownMedium && <SwiperPrevButton swiperRef={swiperRef} />}
-      <Swiper
-        className='main-swiper'
-        onBeforeInit={(swiper) => {
-          swiperRef.current = swiper;
+    <Box
+      sx={{
+        width: layoutWidth,
+        display: 'flex',
+        gap: 2,
+        alignItems: 'center',
+        justifyContent: 'space-between',
+      }}
+    >
+      <Box>data box</Box>
+      <Box
+        sx={{
+          display: 'flex',
+          gap: 2,
+          alignItems: 'center',
+          '& .swiper-wrapper': {
+            width: slideWrapperWidth,
+          },
         }}
-        slidesPerView={slidePerView}
-        modules={[Autoplay, Mousewheel, Navigation, Pagination]}
-        // autoplay
-        mousewheel
-        loop
-        pagination={{ clickable: true }}
       >
-        {isLoading && <Skeleton width='100%' height='100%' />}
-        {!isLoading &&
-          recommendedStores?.data.map((storeData, index) => (
-            <SwiperSlide key={'recommended-store-' + index}>
-              <VerticalStoreCard
-                storeData={storeData}
-                onCardClick={onCardClick}
-              />
-            </SwiperSlide>
-          ))}
-      </Swiper>
-      {!isDownMedium && <SwiperNextButton swiperRef={swiperRef} />}
-      <ProgressBackdrop open={isBackdrop} />
-    </>
+        {!isDownMedium && <SwiperPrevButton swiperRef={swiperRef} />}
+        <Swiper
+          className='main-swiper'
+          effect={'flip'}
+          onBeforeInit={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          grabCursor
+          centeredSlides
+          modules={[Autoplay, Mousewheel, Navigation, Pagination, EffectFlip]}
+          autoplay
+          mousewheel
+          loop
+          pagination={{ clickable: true }}
+        >
+          {isLoading && <Skeleton width='100%' height='100%' />}
+          {!isLoading &&
+            recommendedStores?.data.map((storeData, index) => (
+              <SwiperSlide key={'recommended-store-' + index}>
+                <VerticalStoreCard
+                  storeData={storeData}
+                  onCardClick={onCardClick}
+                />
+              </SwiperSlide>
+            ))}
+        </Swiper>
+        {!isDownMedium && <SwiperNextButton swiperRef={swiperRef} />}
+        <ProgressBackdrop open={isBackdrop} />
+      </Box>
+    </Box>
   );
 };
 
