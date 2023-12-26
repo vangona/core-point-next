a1a9895723fcb92d25d5c860c705e547d95760ab
vangona <rlarhksrud12@naver.com>
2023-12-27 00:12:44 +0900

    chore: 섹션별 설명 예시 추가

--- src/components/common/section-title/SectionTitle.tsx
+++ src/components/common/section-title/SectionTitle.tsx
@@ -3,17 +3,22 @@ import Typography from '@mui/material/Typography';
 
 interface SectionTitleProps {
   label?: React.ReactNode;
+  subTitle?: React.ReactNode;
 }
 const SectionTitle = (props: SectionTitleProps) => {
-  const { label } = props;
+  const { label, subTitle } = props;
   return (
-    <Typography
-      variant='h5'
-      component='h2'
-      sx={{ margin: 5, fontWeight: 'bold' }}
-    >
-      {label}
-    </Typography>
+    <>
+      <Typography
+        variant='h5'
+        component='h2'
+        sx={{ margin: subTitle ? 3 : 5 }}
+        fontFamily='ONE-Mobile-Title'
+      >
+        {label}
+      </Typography>
+      {subTitle && <Typography sx={{ mb: 5 }}>{subTitle}</Typography>}
+    </>
   );
 };
 
